b894306475e18029c77bdd8392c81222c45dd3d3
lucask0 <lucas.koleff@usp.br>
2020-09-11 21:27:45 -0300

    Finish implementation of missing functions

--- sliding_mode.c
+++ sliding_mode.c
@@ -6,7 +6,19 @@
 #define k 0.0f // equivalent "elasticity"
 #define lambda 0.0f // decay rate
 #define f_s 20000.0f // sampling frequency
-#define T_s 1/f_s // sampling time
+#define T_s 1.0f/f_s // sampling time
+#define eta 0.1f*1/300.0f // define eta as a fraction of signal frequency
+
+// Define uncertainties
+#define Lplus 1.1f*L;
+#define Lminus 0.9f*L;
+#define Rplus 1.1f*R;
+#define Rminus 0.9f*R;
+#define kplus 1.1f*k;
+#define kminus 0.9f*k;
+
+#define d (Lplus - Lminus)/Lminus; // d according to (14)
+
 
 // Global variables
 static float u; // Control effort
@@ -16,6 +28,7 @@ static float i; // Measured current
 static float q; // Measured charge
 static float d_i_star; // First-order derivative of reference current
 static float d_qtilde; // First-order derivatide of tilde measured charge
+static float d_itilde; // First-order derivative of
 
 // Function declarations
 void update_i();
@@ -23,6 +36,7 @@ void update_q();
 void update_d_i_star();
 void update_d_qtilde();
 void update_epsilon();
+void update_d_itilde();
 void update_K_big();
 
 
@@ -47,6 +61,7 @@ void calculate_u()
     update_d_i_star();
     update_d_qtilde();
     update_epsilon();
+    update_d_itilde();
     update_K_big();
 
     // Calculate control effort acc. to (4)
@@ -103,7 +118,7 @@ void update_q()
 
 }
 
-void d_i_star()
+void update_d_i_star()
 {
     // This function updates the value of d_i_star by calculating the first derivative
     // reference current
@@ -144,4 +159,118 @@ void d_i_star()
     // calculate derivative output
     d_i_star = (i_star - i_star_old) / T_s;
 
+}
+
+
+void update_d_qtilde()
+{
+    // This function updates the value of d_qtilde by calculating the first derivative
+    // of the estimated charge
+    //
+    // OUTPUTS
+    // d_qtilde: derivative of the estimated charge
+    //
+    // INPUTS
+    //
+    // q: charge
+    //
+    // INTERNAL VARIABLES
+    // 
+    // q_old: previous value for the estimated charge
+    
+    static float q_old = 0.0f;
+
+    // store old reference
+    q_old = q;
+
+    // calculate derivative output
+    d_qtilde = (q - q_old) / T_s;
+
+}
+
+void update_d_itilde()
+{
+    // This function updates the value of d_itilde by calculating the first derivative
+    // of the measured/estimated current
+    //
+    // OUTPUTS
+    // d_itilde: derivative of the measured/estimated current
+    //
+    // INPUTS
+    //
+    // i: current
+    //
+    // INTERNAL VARIABLES
+    // 
+    // i_old: previous value for the measured/estimated current
+    
+    static float i_old = 0.0f;
+
+    // store old reference
+    i_old = i;
+
+    // calculate derivative output
+    d_itilde = (i - i_old) / T_s;
+
+}
+
+
+
+void update_epsilon()
+{
+    // This function updates the value of epsilon(s) 
+    //
+    // OUTPUTS
+    // epsilon: derivative of the estimated charge
+    //
+    // INPUTS
+    //
+    // q: charge
+    // i: current
+    //
+    // INTERNAL VARIABLES
+    // 
+    // s: sliding surface
+    
+    float s = 0.0f;
+
+    // first, calculate s according to (2)
+    s = i + lambda * q;
+
+    // apply saturation function on s to calculate epsilon
+
+    if (s>1)
+    {
+        epsilon = 1;
+    } else if (s<-1) 
+    {
+        epsilon = -1;
+    } else {
+        epsilon = s;
+    }
+
+}
+
+
+
+void update_K_big()
+{
+
+    // This function updates the value of epsilon(s) 
+    //
+    // OUTPUTS
+    // K_big
+    //
+    // INPUTS
+    //
+    // q, i, eta, Fs, d, R, L, k, d_i_star, lambda, d_i_tilde
+    // Rplus, Rminus, Lplus, Lminus, kplus, kminus
+    //
+    // INTERNAL VARIABLES
+    // 
+    // Fs
+
+    Fs= fabsf((Rplus*Lplus-Rminus*Lminus))/Lminus/Lminus*fabs(i) + fabsf((kplus*Lplus-kminus*Lminus))/Lminus/Lminus*fabs(q);
+
+    K_big = 1/(1-d) * (eta + Fs + d * fabsf(-R/L*i - k/L*q - d_i_star + lambda*d_i_tilde));
 }
